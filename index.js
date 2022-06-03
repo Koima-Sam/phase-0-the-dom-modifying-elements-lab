590d92798d727e1d0bb5436888ae9714333b509a
Koima-Sam <sam17koima@gmail.com>
2022-06-03 16:48:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//no longer has DOM node 'main#main'
+const main = document.querySelector("main");
+main.remove();
+//has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+//has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.textContent = "SAMMY KOIMA is the champion";
